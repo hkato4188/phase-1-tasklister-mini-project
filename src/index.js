0d953afa9df421e7dbca8cb3a4ed04548b27e70f
Hiro Kato <hirokikato1@gmail.com>
2023-07-14 15:13:18 -0400

    completes lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form = document
+  .getElementById("create-task-form")
+  .addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+
+const submitButton = document
+  .getElementById("submitButton")
+  .addEventListener("click", () => createNewTask());
+
+const createNewToDo = () => {
+  createNewTask();
+  createNewDeleteButton();
+};
+
+function createNewTask() {
+  const formInput = document.getElementById("new-task-description");
+  const priority = document.getElementById("priority-select").value;
+  const node = document.createElement("li");
+  const textNode = document.createTextNode(formInput.value);
+  node.className = `${priority}`;
+  const form = document.getElementById("tasks");
+  node.appendChild(textNode);
+  form.appendChild(node);
+  
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "🗑️";
+  deleteButton.addEventListener("click", () => node.remove());
+  node.appendChild(deleteButton);
+}
